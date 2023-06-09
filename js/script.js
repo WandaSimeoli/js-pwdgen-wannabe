d10cafa61720b67797ec3c7ff07d19ea315bcc23
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-09 17:41:37 +0200

    Alert con form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const name = prompt('Inserisci il tuo nome'); 
 const surname = prompt('Inserisci il tuo cognome');
+const color = prompt('Inserisci il tuo colore preferito');
 
-
-document.getElementById('my-id').innerHTML = name + '' + surname; 
+document.getElementById('my-id').innerHTML = name + '' + surname + '' + color + '' + '101'; 
